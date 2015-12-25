85475f8e7ceea3078f13fd7acb8f730116337ade
Calvin Reibenspieß <calvinreibenspiess@gmail.com>
2015-12-25 01:25:58 +0100

    loading indicator

---
+++ client/views/tachometer/components/centeredSpinner.jsx
@@ -0,0 +1,9 @@
+TachometerCenteredSpinner = React.createClass({
+  render(){
+    return (
+      <div style={{height: "35em", display: "flex", alignItems: "center", justifyContent: "center"}}>
+        <TachometerSpinner/>
+      </div>
+    );
+  }
+});
