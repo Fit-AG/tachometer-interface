7b19d169b048f154ed1512d90bc5b10073476bb4
Calvin Reibenspieß <calvinreibenspiess@gmail.com>
2015-12-25 01:25:29 +0100

    first mock of a notebox
    The idea is it to add a notebox that can be used to write notes about
    each measurement

---
+++ client/views/tachometer/components/noteBox.jsx
@@ -0,0 +1,15 @@
+TachometereNoteBox = React.createClass({
+  render(){
+    return (
+      // <div className="mdl-card overlay__surface">
+      //   <div className="mdl-card__supporting-text">
+      //     <div className="mdl-textfield mdl-js-textfield">
+      //       <textarea className="mdl-textfield__input" type="text" rows= "5" id="sample5"></textarea>
+      //       <label className="mdl-textfield__label" for="sample5">NOTES</label>
+      //     </div>
+      //   </div>
+      // </div>
+      <div/>
+    );
+  }
+});
